3bae73eb1294244690afd0bf0db553c677a2e17d
Sidó Norbert <sido.norbert93@gmail.com>
2024-02-22 10:30:12 +0100

    Upload components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './app.routes';
+
 import { AppComponent } from './app.component';
 import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 import { TemplateFormComponent } from './template-form/template-form.component';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
     ReactiveFormComponent,
-    TemplateFormComponent
+    TemplateFormComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [
     provideClientHydration()
--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrl: './template-form.component.scss'
 })
 export class TemplateFormComponent {
+  percent: string = "";
+  grade: string = "" ;
 
+  submitCalc(form: any){
+    let sum = 0;
+    for(let x in form.controls){
+      sum += form.controls[x].value
+    };
+    
+    let percent = Math.ceil((sum / 150) * 100);
+    this.percent = percent.toFixed(0) + '%';
+    
+    if(percent >= 60) this.grade = 'Jeles (5)'
+    else if(percent >= 47) this.grade = 'Jó (4)'
+    else if(percent >= 33) this.grade = 'Közepes (3)'
+    else if(percent >= 25) this.grade = 'Elégséges (2)'
+    else this.grade = 'Elégtelen (1)'; 
+  }
+
+  resetBtn(form: any){
+    form.controls.textEdit.reset();
+    form.controls.tableManagement.reset();
+    form.controls.dataBaseManagement.reset();
+    form.controls.algoritm.reset();
+    form.controls.oralPoint.reset();
+    this.percent = '';
+    this.grade = '';
+  }
 }
